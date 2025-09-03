8064eee4637422736bce8c71302f6125d9236dad
F4D3L <guicellifadel@gmail.com>
2025-09-03 20:19:24 -0300

    setup inicial do projeto

---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+    # TODO: Implementar lógica
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # TODO: Implementar lógica
+    pass
+
+def encontrar_maior_palavra(frase):
+    # TODO: Implementar lógica
+    pass
