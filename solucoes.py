8ac7ead19280b455db308b0b5b349888d001a9f4
Rafael Zeni <rafaelzenicp@gmail.com>
2025-09-03 20:34:23 -0300

    feat: implementação função anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,7 +1,10 @@
 def sao_anagramas(string1, string2):
-    # TODO: Implementar lógica
-    pass
-
+    palavra_teste = string2[::-1]
+    if palavra_teste == string1:
+        return True
+    else:
+        return False
+    
 def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar lógica
     pass
@@ -9,3 +12,7 @@ def cifra_de_cesar(texto, deslocamento):
 def encontrar_maior_palavra(frase):
     # TODO: Implementar lógica
     pass
+
+
+print(sao_anagramas("roma", "amor"))
+print(sao_anagramas("casa", "arroz"))
